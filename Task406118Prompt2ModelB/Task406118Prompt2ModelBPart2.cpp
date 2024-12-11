04bcdd219286d7ee5c11315dbdb211c1bf966254
qamarali-alt <qamar.a@turing.com>
2024-12-11 07:06:43 +0000

    Populating codes for Prompt 2 for Model B. Generating the appropriate binaries.

---
+++ Task406118Prompt2ModelB/Task406118Prompt2ModelBPart2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+
+int shared_counter(0);
+
+void increment_without_sync() {
+    for (int i = 0; i < 1000000; ++i) {
+        ++shared_counter;
+    }
+}
+
+int main() {
+    std::thread thread1(increment_without_sync);
+    std::thread thread2(increment_without_sync);
+
+    thread1.join();
+    thread2.join();
+
+    std::cout << "Expected counter value: 2000000, Actual: " << shared_counter << std::endl;
+    return 0;
+}
